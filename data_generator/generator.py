0a25c139bec1dbfebe73a6587707ae0a1e2bc95f
Jiri Hruban <jiri@stockstory.org>
2023-05-22 22:03:17 +0200

    add some more fake data

--- data_generator/generator.py
+++ data_generator/generator.py
@@ -5,6 +5,8 @@ import datetime
 from faker import Faker
 from pydantic import BaseModel
 
+MAX_USER_ID = 1_000_000
+MAX_POST_ID = 1_000_000
 FAKER_SEED = 43
 Faker.seed(FAKER_SEED)
 faker_generator = Faker()
@@ -37,8 +39,9 @@ class User(BaseModel):
 class Visit(BaseModel):
     user_id: int
     site_id: int
-    timestamp: int
-    article_id: int
+    timestamp: datetime.datetime
+    post_id: int
+    origin_url: str
     ip: str
     user_agent: str
 
@@ -46,12 +49,73 @@ class Visit(BaseModel):
     def generate(cls) -> Visit:
         return cls.parse_obj(
             {
-                "user_id": faker_generator.pyint(),
-                "site_id": faker_generator.pyint(),
-                "timestamp": make_timestamp(),
-                "article_id": faker_generator.pyint(),
+                "user_id": faker_generator.pyint(max_value=MAX_USER_ID),
+                "site_id": faker_generator.pyint(max_value=3),
+                "timestamp": faker_generator.date_time(),
+                "post_id": faker_generator.pyint(max_value=MAX_POST_ID),
+                "origin_url": faker_generator.url(),
                 "ip": faker_generator.ipv4_public(),
                 "user_agent": faker_generator.user_agent(),
+            }
+        )
+
+
+class Post(BaseModel):
+    user_id: int
+    time_created: datetime.datetime
+    time_updated: datetime.datetime
+    post_id: int
+    title: str
+    content: str
+
+    @classmethod
+    def generate(cls) -> Post:
+        return cls.parse_obj(
+            {
+                "user_id": faker_generator.pyint(max_value=MAX_USER_ID),
+                "time_created": faker_generator.date_time(),
+                "time_updated": faker_generator.date_time(),
+                "post_id": faker_generator.pyint(max_value=MAX_POST_ID),
+                "title": faker_generator.text(),
+                "content": faker_generator.text(),
+            }
+        )
+
+
+class Uploads(BaseModel):
+    post_id: int
+    file_name: str
+
+    @classmethod
+    def generate(cls) -> Uploads:
+        return cls.parse_obj(
+            {
+                "post_id": faker_generator.pyint(max_value=MAX_POST_ID),
+                "file_name": faker_generator.file_name(),
+            }
+        )
+
+
+class Comment(BaseModel):
+    user_id: int
+    time_created: datetime.datetime
+    time_updated: datetime.datetime
+    post_id: int
+    content: str
+    upvotes_count: int
+    downvotes_count: int
+
+    @classmethod
+    def generate(cls) -> Comment:
+        return cls.parse_obj(
+            {
+                "user_id": faker_generator.pyint(),
+                "time_created": faker_generator.date_time(),
+                "time_updated": faker_generator.date_time(),
+                "post_id": faker_generator.pyint(),
+                "content": faker_generator.text(),
+                "upvotes_count": faker_generator.pyint(),
+                "downvotes_count": faker_generator.pyint(),
             }
         )
 
@@ -59,3 +123,4 @@ class Visit(BaseModel):
 if __name__ == "__main__":
     print(User.generate())
     print(Visit.generate())
+    print(Post.generate())
